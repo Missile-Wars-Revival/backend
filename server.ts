3b70a162d46717315b3f778f62fe60ec316dc66e
Long Time No C <85569855+longtimeno-c@users.noreply.github.com>
2024-04-23 11:59:53 +0100

    added function to handle retriving other players data

--- server.ts
+++ server.ts
@@ -16,7 +16,7 @@ app.post('/api/login', (req, res) => {
         res.status(401).send('invalid username or password');
     }
 });
-
+//send their own location 
 app.post('/api/sendLocation', (req, res) => {
     const { username, latitude, longitude } = req.body;
     const existingLocationIndex = playerLocations.findIndex(loc => loc.username === username);
@@ -34,6 +34,10 @@ app.post('/api/sendLocation', (req, res) => {
 
 app.get('/api/protected', (req, res) => {
     res.send('protected content');
+});
+//fetch location of other players
+app.get('/api/getOtherPlayersData', (req, res) => {
+    res.json(playerLocations);
 });
 
 app.listen(3000, () => {
