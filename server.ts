51c281a968954c14057322b67e355a45ddb313c0
Tristan Hill <tphwoodlands@gmail.com>
2024-09-05 11:18:32 +0100

    patche

--- server.ts
+++ server.ts
@@ -63,16 +63,16 @@ setInterval(deleteExpiredLandmines, 30000);
 setInterval(deleteExpiredLoot, 30000);
 setInterval(updateMissilePositions, 30000);
 //player notificaitons
-//setInterval(checkPlayerProximity, 15000);
+setInterval(checkPlayerProximity, 15000);
 
 //manages notifications
-//startNotificationManager();
+startNotificationManager();
 
 //manages leagues
 leagueRunner;
 
 //Bots:
-//manageAIBots();
+manageAIBots();
 
 //deleteAllBots();
 
