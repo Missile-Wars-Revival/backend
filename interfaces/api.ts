13f0a9119256f7028f217f455e0677482136d697
Manaf941 <manaf@2221.ch>
2024-10-14 18:38:38 +0200

    body schema for authRoutes.ts

--- interfaces/api.ts
+++ interfaces/api.ts
@@ -1,18 +1,5 @@
 import { z } from "zod";
 
-const LoginSchema = z.object({
-  username: z.string(),
-  password: z.string(),
-  notificationToken: z.string().optional(),
-});
-
-const RegisterSchema = z.object({
-  username: z.string(),
-  email: z.string(),
-  password: z.string(),
-  notificationToken: z.string(),
-});
-
 const AuthWithLocationSchema = z.object({
   token: z.string(),
   latitude: z.string(),
@@ -24,16 +11,12 @@ const AuthForFriendSchema = z.object({
   friend: z.string(),
 });
 
-type Login = z.infer<typeof LoginSchema>;
-type Register = z.infer<typeof RegisterSchema>;
 type AuthWithLocation = z.infer<typeof AuthWithLocationSchema>;
 type AuthForFriend = z.infer<typeof AuthForFriendSchema>;
 
 export {
-  LoginSchema,
-  RegisterSchema,
   AuthWithLocationSchema,
   AuthForFriendSchema,
 };
 
-export type { Login, Register, AuthWithLocation, AuthForFriend };
+export type { AuthWithLocation, AuthForFriend };
--- utils/schema.ts
+++ utils/schema.ts
@@ -1,17 +1,19 @@
 import { NextFunction, Request, Response } from "express";
-import { ZodError, ZodSchema } from "zod";
+import { z, ZodError, ZodSchema } from "zod";
 
 export function validateSchema(schema: ZodSchema) {
     return async function(req: Request, res: Response, next: NextFunction) {
         try {
-            req.body = schema.parseAsync(req.body);
+            req.body = await schema.parseAsync(req.body);
             next();
         } catch (error) {
             if (error instanceof ZodError) {
                 return res.status(400).json(error.errors);
             }
-            
+
             next(error); // Pass the error to the next error handler
         }
     }
 }
+
+export const emailSchema = z.string().email()
